fce5605f2a6d0f135a8a5beb6daecf24a84bec74
Gaston <gaston.herrerabaron@outlook.com>
2021-06-27 00:01:36 -0300

    modifciaciones package 2

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const app = express();
 
-//middlewares
+const app = express(); //middlewares
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+})); //routes
 
-//routes
 app.use(require('./routers/index'));
 app.get("/", (req, res) => {
-    res.json({ message: "Hello from server!!" });
+  res.json({
+    message: "Hello from server!!"
+  });
 });
-
 app.listen(3000);
 console.log('Server on port 3000');
--- dist/routers/index.js
+++ dist/routers/index.js
@@ -1,12 +1,20 @@
-const { Router } = require('express');
+const {
+  Router
+} = require('express');
+
 const router = Router();
 
-const { getUsers, createUsers, getUsersById, deleteById, updateById } = require('../controllers/index.controller');
+const {
+  getUsers,
+  createUsers,
+  getUsersById,
+  deleteById,
+  updateById
+} = require('../controllers/index.controller');
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUsersById);
 router.post('/users', createUsers);
 router.delete('/users/:id', deleteById);
 router.put('/users/:id', updateById);
-
 module.exports = router;
