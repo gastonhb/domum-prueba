1f818e4f5977ef59de72ffaaefa45af670440710
Andres <andresfernandez.1197@gmail.com>
2021-06-28 18:21:44 -0300

    cambio

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express(); //middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+})); //routes
+
+app.use(require('./routers/index'));
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from server!!"
+  });
+});
+app.listen(3000);
+console.log('Server on port 3000');
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+const puerto = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.use(require('./routers/index'))
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!!" });
+    }); 
+
+app.listen(puerto);
+console.log('Server on port 3000')
